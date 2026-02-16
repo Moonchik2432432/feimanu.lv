4b12e8af9fe5409dc8e98564c6e0c351c7c95ccb
Ruslans <rusvor2005@gmail.com>
2026-02-17 00:18:46 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('headerCategories', Kategorija::orderBy('nosaukums')->get());
-
-        Paginator::useBootstrap();
     }
 }
