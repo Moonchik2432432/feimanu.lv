af3f330bcaf40bc974597a47ffa7dd3ec6b520d0
Ruslans <rusvor2005@gmail.com>
2026-02-16 21:38:24 +0200

    r

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,17 +1,36 @@
 <header class="site-header">
     <div class="container">
 
-    <div class="logo">
-        <a href="{{ url('/') }}">
-            <img src="public/img/logo-feimanu.jpg" alt="Feimanu Logo">
-        </a>
-    </div>
+        <div class="logo">
+            <a href="{{ url('/') }}">
+                <img src="{{ asset('img/logo-feimanu.jpg') }}" alt="Feimanu Logo">
+            </a>
+        </div>
 
         <nav class="nav">
-            <a href="{{ route('aktualitates.index') }}">Aktualitātes</a>
+
+            <div class="dropdown">
+                <a href="{{ route('aktualitates.index') }}" class="dropbtn">
+                    Aktualitātes
+                </a>
+
+                <div class="dropdown-content">
+                    <a href="{{ route('aktualitates.index') }}">
+                        Visas aktualitātes
+                    </a>
+
+                    @foreach($headerCategories as $cat)
+                        <a href="{{ route('aktualitates.category', $cat->kategorija_id) }}">
+                            {{ $cat->nosaukums }}
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+
             <a href="#">Ziņas</a>
             <a href="#">Par mums</a>
             <a href="#">Kontakti</a>
+
         </nav>
     </div>
 </header>
