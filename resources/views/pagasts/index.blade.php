e7163842d1303ad55019d54b69d9d14fa494c5e9
Ruslans <rusvor2005@gmail.com>
2026-02-16 23:32:11 +0200

    Update index.blade.php

--- resources/views/pagasts/index.blade.php
+++ resources/views/pagasts/index.blade.php
@@ -6,5 +6,6 @@
 <div class="container">
   <h1>Pagasts</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto obcaecati libero ea, velit fugiat illo maiores delectus, facere ipsa deserunt ex vitae voluptatem quis rem incidunt! Deleniti omnis aut suscipit.</p>
+  <h2>Par mums</h2>
 </div>
 @endsection
