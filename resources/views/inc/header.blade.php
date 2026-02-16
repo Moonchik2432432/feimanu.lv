aca59434217a971cd8bd728481eb8f1a64d98aa5
Ruslans <rusvor2005@gmail.com>
2026-02-16 21:47:21 +0200

    f

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,36 +1,29 @@
 <header class="site-header">
     <div class="container">
 
-        <div class="logo">
-            <a href="{{ url('/') }}">
-                <img src="{{ asset('img/logo-feimanu.jpg') }}" alt="Feimanu Logo">
-            </a>
-        </div>
-
+    <div class="logo">
+        <a href="{{ url('/') }}">
+            <img src="public/img/logo-feimanu.jpg" alt="Feimanu Logo">
+        </a>
+    </div>
         <nav class="nav">
+            <div class="menu">
+                <a href="{{ route('aktualitates.index') }}">Aktualitātes</a>
 
-            <div class="dropdown">
-                <a href="{{ route('aktualitates.index') }}" class="dropbtn">
-                    Aktualitātes
-                </a>
+                <div class="submenu">
+                <a href="{{ route('aktualitates.index') }}">Visas aktualitātes</a>
 
-                <div class="dropdown-content">
-                    <a href="{{ route('aktualitates.index') }}">
-                        Visas aktualitātes
+                @foreach($headerCategories as $cat)
+                    <a href="{{ route('aktualitates.category', $cat->kategorija_id) }}">
+                    {{ $cat->nosaukums }}
                     </a>
-
-                    @foreach($headerCategories as $cat)
-                        <a href="{{ route('aktualitates.category', $cat->kategorija_id) }}">
-                            {{ $cat->nosaukums }}
-                        </a>
-                    @endforeach
+                @endforeach
                 </div>
             </div>
 
             <a href="#">Ziņas</a>
             <a href="#">Par mums</a>
             <a href="#">Kontakti</a>
-
         </nav>
     </div>
 </header>
