2ef23fd66086e2114d5d559fbe62c88123c566a6
Ruslans <rusvor2005@gmail.com>
2026-02-16 21:12:03 +0200

    Update show.blade.php

--- resources/views/aktualitates/show.blade.php
+++ resources/views/aktualitates/show.blade.php
@@ -5,10 +5,23 @@
 @section('content')
 <div class="container">
   <h1>{{ $post->nosaukums }}</h1>
+
   <div style="color:#666;margin:10px 0;">
     {{ \Carbon\Carbon::parse($post->publicets_datums)->format('d.m.Y H:i') }}
     @if($post->kategorija) • {{ $post->kategorija->nosaukums }} @endif
   </div>
-  <div>{!! nl2br(e($post->saturs)) !!}</div>
+
+  {{-- Картинка --}}
+  @if($post->bilde)
+      <img 
+          src="{{ asset($post->bilde) }}"
+          alt="{{ $post->nosaukums }}"
+          style="max-width:600px; width:100%; border-radius:12px; margin:20px 0;"
+      >
+  @endif
+
+  <div>
+      {!! nl2br(e($post->saturs)) !!}
+  </div>
 </div>
 @endsection
