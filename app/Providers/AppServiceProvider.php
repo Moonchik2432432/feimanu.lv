2219b6fb72cd34fb706e7af868c53e31bbd3f701
Ruslans <rusvor2005@gmail.com>
2026-03-10 18:10:35 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use App\Models\Kategorija;
+use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::share('headerCategories', Kategorija::orderBy('nosaukums')->get());
+        View::share('headerCategories', Category::orderBy('nosaukums')->get());
     }
 }
