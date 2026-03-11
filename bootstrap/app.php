545f6423ff4844501d5c799d7c3ab74b003c9814
Ruslans <rusvor2005@gmail.com>
2026-03-11 20:51:27 +0200

    Update app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'blocked' => \App\Http\Middleware\CheckIfBlocked::class,
         ]);
+
+        $middleware->appendToGroup('web', \App\Http\Middleware\CheckIfBlocked::class);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
