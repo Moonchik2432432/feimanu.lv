6784ec0f26d7e39d0e6a7a32d0ccd29a837e8c66
Moonchik2432432 <rusvor2005@gmail.com>
2026-03-19 12:32:58 +0200

    r

--- js/calendar.js
+++ js/calendar.js
@@ -1,12 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    flatpickr("#from", {
+    flatpickr(".date-picker", {
         dateFormat: "Y-m-d",
         locale: "lv"
     });
-    
-    flatpickr("#to", {
-        dateFormat: "Y-m-d",
-        locale: "lv"
-    });
-})
+});
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -34,12 +34,12 @@
  
         <div>
             <label>No: </label><br>
-            <input type="text" id="from" name="from" value="{{ $from ?? '' }}" style="padding:8px;">
+            <input type="text" id="from" name="from" class="date-picker" value="{{ $from ?? '' }}" style="padding:8px;">
         </div>
  
         <div>
             <label>Līdz: </label><br>
-            <input type="text" id="to" name="to" value="{{ $to ?? '' }}" style="padding:8px;">
+            <input type="text" id="to" name="to" class="date-picker" value="{{ $to ?? '' }}" style="padding:8px;">
         </div>
  
         <button type="submit" style="padding:9px 14px;">Filtrēt</button>
