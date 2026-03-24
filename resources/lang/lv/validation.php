80e0ce3e976ac997a8359f100d6861ef72309d20
Moonchik2432432 <rusvor2005@gmail.com>
2026-03-24 10:20:21 +0200

    Create validation.php

---
+++ resources/lang/lv/validation.php
@@ -0,0 +1,18 @@
+<?php
+ 
+return [
+ 
+    'required' => 'Lauks :attribute ir obligāts.',
+    'image' => 'Lauks :attribute jābūt attēlam.',
+    'max' => [
+        'file' => 'Fails :attribute nedrīkst būt lielāks par :max KB.',
+    ],
+ 
+    'attributes' => [
+        'bilde' => 'attēls',
+        'nosaukums' => 'nosaukums',
+        'saturs' => 'saturs',
+        'kategorija_id' => 'kategorija',
+    ],
+ 
+];
