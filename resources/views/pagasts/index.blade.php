96ea4cf6056eae9a9acb3c892f876f07d24fa816
Moonchik2432432 <rusvor2005@gmail.com>
2026-03-25 15:51:22 +0200

    Update index.blade.php

--- resources/views/pagasts/index.blade.php
+++ resources/views/pagasts/index.blade.php
@@ -13,15 +13,15 @@
         </p>
 
         <p style="line-height:1.7;">
-            Feimaņu pagasts ir vieta, kur sastopas Latgales dabas skaistums, vietējās tradīcijas un vēsturiskais mantojums. Apkārtne ir pazīstama ar ezeriem, lauku ainavām un mierīgu vidi, kas ir nozīmīga gan vietējiem iedzīvotājiem, gan viesiem. Oficiālajā Rēzeknes novada informācijā Feimaņu pagasts raksturots kā teritorija ar skaistām dabas ainavām Latgales augstienē. :contentReference[oaicite:1]{index=1}
+            Feimaņu pagasts ir vieta, kur sastopas Latgales dabas skaistums, vietējās tradīcijas un vēsturiskais mantojums. Apkārtne ir pazīstama ar ezeriem, lauku ainavām un mierīgu vidi, kas ir nozīmīga gan vietējiem iedzīvotājiem, gan viesiem. Oficiālajā Rēzeknes novada informācijā Feimaņu pagasts raksturots kā teritorija ar skaistām dabas ainavām Latgales augstienē. 
         </p>
 
         <p style="line-height:1.7;">
-            Pagasta teritorijā atrodas vairākas apdzīvotās vietas un kultūrvēsturiski objekti. Feimaņu pagasts ir saistīts arī ar vietējo kultūras dzīvi, draudzes tradīcijām un apkārtnes vēsturisko attīstību. Publiski pieejamajos avotos minēti arī tādi ar pagastu saistīti objekti kā Feimaņu baznīca, Feimaņu ezers un citi apkārtnes kultūrvēsturiskie punkti. :contentReference[oaicite:2]{index=2}
+            Pagasta teritorijā atrodas vairākas apdzīvotās vietas un kultūrvēsturiski objekti. Feimaņu pagasts ir saistīts arī ar vietējo kultūras dzīvi, draudzes tradīcijām un apkārtnes vēsturisko attīstību. Publiski pieejamajos avotos minēti arī tādi ar pagastu saistīti objekti kā Feimaņu baznīca, Feimaņu ezers un citi apkārtnes kultūrvēsturiskie punkti.
         </p>
 
         <p style="line-height:1.7;">
-            Feimaņu pagasts ietilpst Maltas apvienības pārvaldes teritorijā, un iedzīvotājiem ir pieejama aktuālā informācija par pakalpojumiem, kalendāru, aktualitātēm un vietējo dzīvi Rēzeknes novada pašvaldības mājaslapā. Pagastam ir arī savs informatīvais izdevums “Feimaņu Zvans”, kurā tiek publicēta vietējā informācija un jaunumi. :contentReference[oaicite:3]{index=3}
+            Feimaņu pagasts ietilpst Maltas apvienības pārvaldes teritorijā, un iedzīvotājiem ir pieejama aktuālā informācija par pakalpojumiem, kalendāru, aktualitātēm un vietējo dzīvi Rēzeknes novada pašvaldības mājaslapā. Pagastam ir arī savs informatīvais izdevums “Feimaņu Zvans”, kurā tiek publicēta vietējā informācija un jaunumi. 
         </p>
 
         <p style="line-height:1.7; margin-bottom:0;">
