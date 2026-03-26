6d6a03fc44b36f223b03276db790e7514673f043
Ruslans Voronovs <rusvor2005@gmail.com>
2026-03-26 15:10:37 +0200

    Adjust layout and styling for contacts index page

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Administrācija - Kontakti')
 
 @section('content')
-<div class="container" style="max-width:1000px; margin:40px auto;">
+<div class="container" style="max-width:1050px; margin:40px auto;">
 
     <h1>Lietotāju jautājumi</h1>
 
