9bebb563e1e75bfc1c24bd86ff203e3d3761f795
Ruslans Voronovs <rusvor2005@gmail.com>
2026-03-30 20:32:11 +0300

    Fix typo in validation rule for 'bilde' field

--- app/Http/Controllers/Admin/AdminNewsController.php
+++ app/Http/Controllers/Admin/AdminNewsController.php
@@ -107,7 +107,7 @@ class AdminNewsController extends Controller
             'nosaukums' => ['required', 'string', 'max:55'],
             'saturs' => ['required', 'string'],
             'kategorija_id' => ['required', 'integer', 'exists:kategorija,kategorija_id'],
-            'bilde' => ['nullabe', 'image', 'mimes:jpg,jpeg,png,webp', 'max:4096'],
+            'bilde' => ['nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:4096'],
         ], [
             'required' => 'Lauks :attribute ir obligāts.',
             'string' => 'Laukam :attribute jābūt tekstam.',
