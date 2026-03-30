2a5633c3ca1b2c76fdf457e416de66a6e28971ed
Ruslans Voronovs <rusvor2005@gmail.com>
2026-03-30 20:55:06 +0300

    Change delete and update routes to use PUT method

--- routes/web.php
+++ routes/web.php
@@ -115,15 +115,15 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/gallery-albums/create', [AdminGalleryAlbumController::class, 'create'])->name('admin.gallery.albums.create');
     Route::post('/gallery-albums/store', [AdminGalleryAlbumController::class, 'store'])->name('admin.gallery.albums.store');
     Route::get('/gallery-albums/{id}/edit', [AdminGalleryAlbumController::class, 'edit'])->name('admin.gallery.albums.edit');
-    Route::post('/gallery-albums/{id}/update', [AdminGalleryAlbumController::class, 'update'])->name('admin.gallery.albums.update');
-    Route::post('/gallery-albums/{id}/delete', [AdminGalleryAlbumController::class, 'destroy'])->name('admin.gallery.albums.delete');
+    Route::put('/gallery-albums/{id}/update', [AdminGalleryAlbumController::class, 'update'])->name('admin.gallery.albums.update');
+    Route::put('/gallery-albums/{id}/delete', [AdminGalleryAlbumController::class, 'destroy'])->name('admin.gallery.albums.delete');
 
     Route::get('/gallery-images', [AdminGalleryImageController::class, 'index'])->name('admin.gallery.images');
     Route::get('/gallery-images/create', [AdminGalleryImageController::class, 'create'])->name('admin.gallery.images.create');
     Route::post('/gallery-images/store', [AdminGalleryImageController::class, 'store'])->name('admin.gallery.images.store');
     Route::get('/gallery-images/{id}/edit', [AdminGalleryImageController::class, 'edit'])->name('admin.gallery.images.edit');
-    Route::post('/gallery-images/{id}/update', [AdminGalleryImageController::class, 'update'])->name('admin.gallery.images.update');
-    Route::post('/gallery-images/{id}/delete', [AdminGalleryImageController::class, 'destroy'])->name('admin.gallery.images.delete');
+    Route::put('/gallery-images/{id}/update', [AdminGalleryImageController::class, 'update'])->name('admin.gallery.images.update');
+    Route::put('/gallery-images/{id}/delete', [AdminGalleryImageController::class, 'destroy'])->name('admin.gallery.images.delete');
 
     //CONTACT
     Route::get('/contacts', [AdminContactController::class, 'index'])->name('admin.contacts');
