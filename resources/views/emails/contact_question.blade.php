6f702d069fca6480d5c0c8ad2555465abb986506
Ruslans Voronovs <rusvor2005@gmail.com>
2026-03-30 22:21:07 +0300

    Update email template for contact questions

--- resources/views/emails/contact_question.blade.php
+++ resources/views/emails/contact_question.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="lv">
-<head>
-    <meta charset="UTF-8">
-    <title>Jauns jautājums</title>
-</head>
-<body style="font-family: Arial, sans-serif; color:#222; line-height:1.6;">
-    <h2>Jauns jautājums no kontaktformas</h2>
+<h2>Jauns ziņojums no mājaslapas</h2>
 
-    <p><strong>Vārds un uzvārds:</strong> {{ $contactMessage->name }}</p>
-    <p><strong>E-pasts:</strong> {{ $contactMessage->email }}</p>
-    <p><strong>Tēma:</strong> {{ $contactMessage->subject }}</p>
+<p><strong>Vārds:</strong> {{ $messageData->name }}</p>
+<p><strong>E-pasts:</strong> {{ $messageData->email }}</p>
+<p><strong>Tēma:</strong> {{ $messageData->subject }}</p>
 
-    <p><strong>Ziņojums:</strong></p>
-    <p>{!! nl2br(e($contactMessage->message)) !!}</p>
-</body>
-</html>
+<p><strong>Ziņojums:</strong></p>
+<p>{{ $messageData->message }}</p>
