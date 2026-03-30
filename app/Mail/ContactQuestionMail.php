eba60bf4806f98a1d1d35c816b3dc79dbdf2588f
Ruslans Voronovs <rusvor2005@gmail.com>
2026-03-30 22:20:24 +0300

    Refactor ContactQuestionMail to use messageData

--- app/Mail/ContactQuestionMail.php
+++ app/Mail/ContactQuestionMail.php
@@ -11,17 +11,16 @@ class ContactQuestionMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public ContactMessage $contactMessage;
+    public $messageData;
 
-    public function __construct(ContactMessage $contactMessage)
+    public function __construct(ContactMessage $messageData)
     {
-        $this->contactMessage = $contactMessage;
+        $this->messageData = $messageData;
     }
 
-    public function build(): self
+    public function build()
     {
-        return $this
-            ->subject('Jauns jautājums no kontaktformas: ' . $this->contactMessage->subject)
+        return $this->subject('Jauns kontakta ziņojums')
             ->view('emails.contact_question');
     }
 }
