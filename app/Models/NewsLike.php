d6c42c2735756d6f23a4463b463eec9f2089cfd2
Ruslans Voronovs <rusvor2005@gmail.com>
2026-04-13 01:44:53 +0300

    Create NewsLike.php

---
+++ app/Models/NewsLike.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsLike extends Model
+{
+    protected $table = 'news_likes';
+
+    protected $fillable = [
+        'user_id',
+        'ieraksts_id',
+    ];
+}
