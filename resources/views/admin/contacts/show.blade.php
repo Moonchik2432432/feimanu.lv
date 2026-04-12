131b9048865599bb8665f3b6d0d809df2794c10a
Ruslans Voronovs <rusvor2005@gmail.com>
2026-04-13 00:49:31 +0300

    Update show.blade.php

--- resources/views/admin/contacts/show.blade.php
+++ resources/views/admin/contacts/show.blade.php
@@ -38,21 +38,28 @@
         <p><strong>Tēma:</strong> {{ $message->subject }}</p>
         <p>
             <strong>Statuss:</strong>
-            <span style="
-                display:inline-block;
-                padding:4px 10px;
-                border-radius:999px;
-                font-size:13px;
-                background:
-                    @if($message->status === 'new') #e8f0ff
-                    @elseif($message->status === 'in_progress') #fff4db
-                    @elseif($message->status === 'answered') #e9ffe9
-                    @else #f0f0f0
-                    @endif;
-                color:#333;
-            ">
-                {{ $statuses[$message->status] ?? $message->status }}
-            </span>
+                <span style="
+                    display:inline-block;
+                    padding:4px 10px;
+                    border-radius:999px;
+                    font-size:13px;
+                    background:
+                        @if($message->status === 'new')
+                            #e8f0ff
+                        @elseif($message->status === 'read')
+                            #fff4db
+                        @elseif($message->status === 'overdue')
+                            #ffe5e5
+                        @elseif($message->status === 'answered')
+                            #e9ffe9
+                        @else
+                            #f0f0f0
+                        @endif
+                    ;
+                    color:#333;
+                ">
+                    {{ $statuses[$item->status] ?? $item->status }}
+                </span>
         </p>
         <p><strong>Datums:</strong> {{ $message->created_at->format('d.m.Y H:i') }}</p>
         <p><strong>Ziņojums:</strong><br>{!! nl2br(e($message->message)) !!}</p>
